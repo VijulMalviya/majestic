b482684df34e084de9ae82115ebbdd96e0b2ea86
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-09-10 19:30:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+AOS.init();
+feather.replace()
+$('.my-element').paroller();
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+
+
+
+
+
+// $(window).on("load", function () {
+//   $(".loader").fadeOut(1500);
+// });
+
+// $(document).ready(function () {
+//   $("#menubar").click(function () {
+//     $(".nav-links").slideToggle("slow");
+//   });
+// });
+
+// var mq = window.matchMedia("(max-width: 576px)");
+// if (mq.matches) {
+//   $(".nav-links ul li").click(function () {
+//     $(".nav-links").slideToggle("slow");
+//   });
+// }
+
+
+
+
+
+
+
+  
+
